a7fb6e05006174bee71825b69578b928293f790b
Gaurav <gaurav.narayanan0910@gmail.com>
2022-10-28 00:45:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import Home from './routes/Home.js';
-import MimicScreen from './routes/MimicScreen/MimicScreen';
-import { Routes, Route } from 'react-router-dom';
+import "./App.css";
+import React, { Component } from "react";
+import Home from "./routes/Home.js";
+import MimicScreen from "./routes/MimicScreen/MimicScreen";
+import BarGraphScreen from "./routes/BarGraph/BarGraphScreen";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className='app-main'>
+    <div className="app-main">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/mimicScreen" element={<MimicScreen />} />
-        {/* <Route path="/barGraph" element={<BarGraph />} />
-        <Route path="/frequencySpectrum" element={<FrequencySpectrum />} />
+        <Route path="/barGraph" element={<BarGraphScreen />} />
+        {/* <Route path="/frequencySpectrum" element={<FrequencySpectrum />} />
         <Route path="/realtimeTrend" element={<RealtimeTrend />} />
         <Route path="/sensorProfile" element={<SensorProfile />} />
         <Route path="/sensorSettings" element={<SensorSettings />} />
